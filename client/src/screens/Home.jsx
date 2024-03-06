b8c33f55c04a859dff45e0e73b6242c6d37cd4eb
Pritish <bordoloipritish97@gmail.com>
2024-03-06 14:07:10 +0530

    test11

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -19,8 +19,8 @@ const Home = () => {
       }
     );
     response = await response.json();
-    setFoodItem(response[1] || []);
-    setFoodCat(response[0] || []);
+    setFoodItem(response[0] || []);
+    setFoodCat(response[1] || []);
   };
 
   useEffect(() => {
